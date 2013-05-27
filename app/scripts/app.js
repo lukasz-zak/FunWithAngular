429a1b6f3974483bff8050df217bde2adb4a46f5
WooCaSh <zlukasz.poczta@gmail.com>
2013-05-27 05:15:48 +0200

    adding some lines to have this same code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('[JS]AngularTestApp', [])
+angular.module('FunWithAngular', ['ui', 'LocalStorageModule'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,37 @@
 'use strict';
 
-angular.module('[JS]AngularTestApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('FunWithAngular')
+    .controller('MainCtrl', function ($scope, localStorageService) {
+    
+    // $scope.todos = [
+    //     'item 1',
+    //     'item 2',
+    //     'item 3'
+    // ];
+    $scope.sortableOptions = {
+        update: function(e, ui) {
+            console.log(ui.item);
+            $('body').css('background-color', 'red');
+            //console.log($scope.todos);
+        },
+        "cursor" : "move"
+    };
+    
+    
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.$watch(function(){
+        console.log($scope.todos[0]);
+        localStorageService.add('todos', $scope.todos.join('\n'));
+    })
+    
+    $scope.addTodo = function(){
+        $scope.todos.push($scope.todo);
+        $scope.todo = '';
+    };
+    
+    $scope.removeItem = function(index){
+        $scope.todos.splice(index, 1);
+    }
+    
   });
